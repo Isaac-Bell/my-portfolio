e298e0eaa7ca748ed8f495e4d9ae1ff03e728e5f
isaac-bell <shabbaa@Shabbaa-Home.>
2025-02-19 01:02:41 +1300

    add Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
-import { SessionProvider } from "next-auth/react";
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
-  return  (
-    <SessionProvider session={session}> 
-    
-    <main className="flex-grow"> <Component {...pageProps} /> </main>;
+import { SessionProvider } from 'next-auth/react'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <main className="flex-grow">
+        {' '}
+        <Component {...pageProps} />{' '}
+      </main>
+      ;
+      <Analytics />
     </SessionProvider>
   )
-  
 }
